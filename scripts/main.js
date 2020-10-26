a019eb342ea502bdde1f10a6fbe05d6eadbe74f9
Gregory Pierot <pierotgregory@gmail.com>
2020-10-25 21:32:27 -0700

    Made changes and added the firebase database for Coffee Run.

---
+++ scripts/main.js
@@ -0,0 +1,39 @@
+(function (window) {
+    'use strict';
+    var FORM_SELECTOR = '[data-coffee-order="form"]';
+    var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]'; // CHOOSE ONLY ONE...
+    var SERVER_URL = 'https://co.audstanley.com/coffeeorders';    // if running on the shared server
+    // var SERVER_URL = 'http://localhost:5500';
+
+    var App = window.App;
+    var Truck = App.Truck;
+    var DataStore = App.DataStore;
+    var RemoteDataStore = App.RemoteDataStore;
+    var FirebaseStore = App.FirebaseStore;
+    var FormHandler = App.FormHandler;
+    var Validation = App.Validation;
+    var CheckList = App.CheckList;
+
+    // var truck = new Truck('ncc-1701', new DataStore());
+    var truck = new Truck('ncc-1701', new RemoteDataStore(SERVER_URL));
+
+    var FirebaseStore2 = new FirebaseStore();
+    var truck2 = new Truck('ncc-1702', FirebaseStore2);
+
+    window.truck = truck;
+    var checkList = new CheckList(CHECKLIST_SELECTOR);
+    checkList.addClickHandler(truck.deliverOrder.bind(truck));
+    var formHandler = new FormHandler(FORM_SELECTOR);
+
+    formHandler.addSubmitHandler(function (data) {
+        return truck.createOrder.call(truck, data)
+            .then(() => {
+                checkList.addRow.call(checkList, data);
+            });
+    });
+    console.log(formHandler);
+
+    formHandler.addInputHandler(Validation.isCompanyEmail);
+    truck.printOrders(checkList.addRow.bind(checkList));
+
+})(window);
